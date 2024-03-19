378579f27437f6737b7d87c266db647c2fd5d708
yasir-shekh <147964633+yasir-shekh@users.noreply.github.com>
2024-03-19 16:05:25 +0530

    content

---
+++ css/style.js
@@ -0,0 +1,298 @@
+// console.log("hello javascript");
+// console.log(879888898, 8955373251, 98989099093);
+// console.log(8955373251);
+// console.warn("hello place inter your name");
+// console.warn("place inter your number");
+// console.error("error");
+// document.write("yasir <i>shekh arafad</i>");
+// document.write("<br>yasir <i>shekh<br>arafad</i>");
+
+// // alert('yasir shekh ok');
+// // alert('ok');
+
+// // document.getElementById("yasir").innerHTML = " <p style="background:red;">hello, javascript</p>";
+// document.getElementById("yasir").innerHTML ="<p>document get eliment by id</p>";
+
+// let a = 10 ;
+// console.log(a);
+
+// let b = 20 + 30;
+// console.log(b);
+
+// let c = 10;
+// let d = 20;
+// console.log(c + d);
+
+// const e = 9000;
+// var f = 10000;
+// console.log(e - f);
+
+// let hello = yasir;
+// console.log( hello);
+
+// let marks = 100993;
+// console.log(typeof marks);
+ 
+// let name = yasir;
+// console.log(typeof "name");
+
+// console.log(typeof undefined);
+// console.log(typeof "k")
+// console.log(typeof "10");
+
+// // confirm("chle kya");
+// // prompt("what is your name?");
+// // alert('helojhjh');
+// // prompt("what is your number");
+
+// const myName = 'sajidali';
+// const dateOf = '3/3/2004';
+// const myEmail = 'yasirshekh321@gmail.com';
+// const address = "balghat";
+// let coching = 'wecode';
+// let number = '8955373251';
+// let phone = 'redmi';
+// const state = "rajathan";
+
+// console.log(myName, dateOf, myEmail, address, coching, number, phone, state); 
+// // ye console me print huya h
+
+// document.write(myName, dateOf,myEmail, address, coching,number,phone, state)
+// // or ye document me print huya h
+
+// console.log(address);
+// document.write(address);
+
+
+// let age = 20;
+// console.log(age);
+
+
+
+// let clientage = prompt ("what is your age");
+// console.log(50 < clientage);
+// // let answear = confirm("what is your age");
+// // console.log(answear);
+
+// // prompt ("Chai pine chlen");
+// // let answer = confirm("Chai pine chlen");
+// // console.log(answer);
+
+
+// let x = 3;
+// let y = 4;
+// // console.log(x++ + y-- * x+y++ +x);
+// console.log(3+4 * 4+3+4);
+
+// let x = 5;
+// let y = 4;
+// console.log(x+y * x+y);
+
+// let y = 4;
+// let x = 2;
+// console.log(x++ +y * y +x);
+ 
+
+
+// if (10 < 20) {
+//   console.log("Yes");
+// } else {
+//   console.log("No");
+// }
+
+
+// if (100 < 20){
+// console.log("yes");
+// }
+// else {
+//     console.log("no");
+// }
+
+
+// const answer = confirm("bha chlenge");
+// if (answer){
+//     alert("chal");
+// }
+// else {
+//     alert("no problem h nhi jana");
+// }
+
+
+// const answear = confirm("tumhara number btau");
+// if (answear){
+//     alert("8955373251");
+// }
+// else{
+//     alert("no problem");
+// }
+
+
+
+
+
+
+
+// let age = prompt("tumhari age kya h");
+// if (age < 40){
+//     console.log("young");
+// }
+// else if (age > 60){
+//     console.log("old");
+// }
+// else {
+// console.log("khuch nhi");
+// }
+ 
+
+// let age = prompt("tumhari age kya h");
+// if ( age < 40){
+//     alert("young");
+// }
+// else if ( age > 60){
+//     alert("old");
+// }
+// else  if ( age > 100);{
+//  alert("sin sitizun");
+// } 
+
+
+// const name = prompt("what is your name?");
+
+// if (name === "yasir"){
+//     console.log("chaye party");
+// } 
+// else if (name === "sharukh"){
+//     console.log("khana party");
+// }
+// else if (name === "rashid"){
+//     console.log("biryani party");
+// }
+// else {
+//     console.log("ghar chlo");
+// }
+
+// const age = prompt("what is your age?");
+// if (age < 22){
+//     alert("yasir");
+// } else if (age < 35){
+//     alert("jakir");
+// } else if (age > 90){
+//     alert("buddha");
+// }
+// else{
+//     alert("nhi");
+// }
+
+// let marks = promt("what are your number");
+
+// if (marks > 70){
+//     console.log("pass");
+// }
+// else{
+//     console.log("fell");
+// }
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+// a = b; 
+// b = c;
+// c = d; 
+// d = a; 
+// console.log(a, b, c, d);
+
+//    console.log(2+5*10-20**2)
+// console.log(20/5*10+10)
+
+// var number = prompt("ak sakhya dale");
+// if ( number % 2 === 0) {
+//     console.log("Yes");
+// } else {
+//     console.log("No");
+// }
+
+
+// let number  = 18
+
+// if (number % 10 === 0 && 5 === 0){
+// console.log("yes");
+// }
+// else {
+//     console.log("no");
+// }
+
+
+// let number = 20;
+// if (number * 20 == 100){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
+
+// let a = 20;
+// let b = 5;
+// let c = 5;
+// let d = 10;
+
+// a = b;
+// b = c;
+// c = d;
+// d = a;
+
+// console.log(a, b, c, d,);
+
+
+// let n = 10;
+// let m = 4;
+
+// let o = m + n;
+// let p = o + o;
+// console.log(p);
+
+// let answear = prompt("whar are u peresenteg");
+
+// let hindi = 90;
+// let SST = 85;
+// let English = 90;
+// let Science = 85;
+// let Math = 90;
+// let total = hindi + SST + English + Science + Math;
+// console.log(total / 500 * 100);
+
+// let hindi = +prompt("hindi");
+// let math = +prompt("math");
+// let Science = +prompt("Science");
+// let english = +prompt("english");
+// let sst = +prompt("sst");
+// let total = hindi + math + Science + english + sst;
+// console.log(total /500 * 100);
+
+// let chaye = confirm("coffe ya chaye + coffe ya chye");
+// let chaye = prompt ("coffe ya chaye");
+// if (chaye){
+//     alert("coffe");
+// }
+// else{
+//     alert("chaye");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
